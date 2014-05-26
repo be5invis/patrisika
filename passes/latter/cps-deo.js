550025e07b6914212fadf425e8a696feb0551073
Belleve Invis <belleve@typeof.net>
2014-05-26 23:02:55 +0800

    Greatly simplified cps-dep

--- passes/latter/cps-deo.js
+++ passes/latter/cps-deo.js
@@ -26,9 +26,9 @@ var plain = syntax_rule(
 	[['.lambda', ',args', ',body'], function(form){
 		var b = plain(this.body);
 		if(b instanceof Array && (b[0] === '&' || b[0] === '&!')) {
-			return ['.plain', ['.lambda', this.args, cps(b, {}, RET)]]
+			return ['.plain', ['.lambda', this.args, re(b, {}, RET)]]
 		} else {
-			return ['.plain', ['.lambda', this.args, re(b, RET)]]
+			return ['.plain', ['.lambda', this.args, re(b, {}, RET)]]
 		}
 	}],
 	[['.quote', ',x'], function(form){ return ['.plain', form] }],
@@ -54,7 +54,7 @@ var plain = syntax_rule(
 				return a
 			}
 		};
-		return ['.plain', a.slice(0, j0).concat(a.slice(j0).map(function(x){ return x[1] }))];
+		return ['.plain', a.slice(0, j0).concat(a.slice(j0))];
 	}],
 	[atom, function(form){ 
 		if(/^\w/.test(form)) return ['.plain', form]
@@ -68,163 +68,73 @@ function id(x){ return x }
 // RS: regularize in statement environment, with return value dropped
 // ctx accepts regularized form, and combines it with incoming subforms.
 var rs = syntax_rule(
-	[['.return', ',x'], function(){ return re(this.x, function(x){
+	[['.return', ',x'], function(form, env, ctx){ return re(this.x, env, function(x){
 		// ctx is not important, and yes, we did DCE here by just simply
 		// drop ctx.
 		return ['.return', x]
 	}) }],
-	[['.if', ',cond', ',consequent'], function(form, ctx){ return re(form.concat([['.unit']]), ctx)}],
-	[['.if', ',cond', ',consequent', ',alternate'], function(form, ctx){
+	[['.if', ',cond', ',consequent'], function(form, env, ctx){ return re(form.concat([['.unit']]), env, ctx)}],
+	[['.if', ',cond', ',consequent', ',alternate'], function(form, env, ctx){
 		var $consequent = this.consequent;
 		var $alternate = this.alternate;
-		return re(this.cond, function(c){
-			return ctx(['.if', c, rs($consequent, id), rs($alternate, id)])
+		return re(this.cond, env, function(c){
+			return ctx(['.if', c, rs($consequent, env, id), rs($alternate, env, id)])
 		})
 	}],
-	[['.while', ',cond', ',body'], function(form, ctx){
+	[['.while', ',cond', ',body'], function(form, env, ctx){
 		if(this.cond instanceof Array && this.cond[0] === '.plain') {
-			return ctx(['.while', this.cond[1], rs(this.body, id)])
+			return ctx(['.while', this.cond[1], rs(this.body, env, id)])
 		} else {
-			return ra(form[1], function(t){
-				return ctx(['.while', t, rs(form[2], function(x){
-					return ['.begin', x, re(form[1], function(x){ return ['=', t, x]})]
+			return ra(form[1], env, function(t){
+				return ctx(['.while', t, rs(form[2], env, function(x){
+					return ['.begin', x, re(form[1], env, function(x){ return ['=', t, x]})]
 				})])
 			})			
 		}
 	}],
-	[['.begin', ',head', ',..rear'], function(form, ctx){
-		return rs(this.head, function(e){
-			return ['.begin', e, rs(['.begin'].concat(form.slice(2)), ctx)]
+	[['.begin', ',head', ',..rear'], function(form, env, ctx){
+		return rs(this.head, env, function(e){
+			return ['.begin', e, rs(['.begin'].concat(form.slice(2)), env, ctx)]
 		})
 	}],
-	[['.begin', ',single'], function(form, ctx){
-		return rs(this.single, ctx)
+	[['.begin', ',single'], function(form, env, ctx){
+		return rs(this.single, env, ctx)
 	}],
-	[['.begin'], function(form, ctx){ return ctx(['.unit']) }],
-	[any, function(form, ctx){ return re(form, ctx) }]
-)
+	[['.begin'], function(form, env, ctx){ return ctx(['.unit']) }],
+	[any, function(form, env, ctx){ return re(form, env, ctx) }]
+);
 
 // RE: regularize in expression environment, keep return value.
 // `ctx` accepts a plain form, often a T-variable, representing
 // the result of evaluating `form`.
 var re = syntax_rule(
-	[empty, function(form){ return ctx['.unit'] }],
-	[['.lambda', ',args', ',body'], function(form, ctx){ return ctx(form) }],
-	[['.quote', ',x'], function(form, ctx){ return ctx(form) }],
-	[['.unit'], function(form, ctx){ return ctx(form) }],
-	[['.plain', ',x'], function(form, ctx){ return ctx(this.x) }],
-	[['.return', ',x'], function(form, ctx){
-		return re(this.x, function(x){ return ['.return', x] })
-	}],
-	[['.if', ',cond', ',consequent'], function(form, ctx){
-		var $consequent = this.consequent;
-		return re(this.cond, function(c){
-			var t = newt();
-			return ['.begin', ['.if', c, re($consequent, function(x){ return ['=', t, x] })], ctx(t)]
-		});
-	}],
-	[['.if', ',cond', ',consequent', ',alternate'], function(form, ctx){
-		var $consequent = this.consequent;
-		var $alternate = this.alternate;
-		return re(this.cond, function(c){
-			var t = newt();
-			return ['.begin', ['.if', c, 
-				re($consequent, function(x){ return ['=', t, x] }),
-				re($alternate, function(x){ return ['=', t, x] })
-			], ctx(t)]
-		});
-	}],
-	[['.while', ',cond', ',body'], function(form, ctx){
-		var $cond = this.cond;
-		var $body = this.body;
-		return ra($cond, function(c){
-			var t = newt();
-			return ['.begin', ['.while', c, re($body, function(x){
-				return ['.begin', ['=', t, x], re($cond, function(x){ return ['=', c, x] })]
-			})], ctx(t)]
-		});
-	}],
-	[	['=', ['.', ',obj', ',field'], ',right'],
-		['=', ['.plain', ['.', ',obj', ',field']], ',right'], 
-			function(form, ctx){
-			var $obj = this.obj, $field = this.field, $right = this.right;
-			return ra($obj, function(xl){
-				return ra($field, function(xr){
-					return re($right, function(r){
-						return ctx(['=', ['.', xl, xr], r])
-					})
-				})
-			})
-		}],
-	[	['=', ['.plain', ',left'], ',right'], 
-		['=', ',left', ',right'], 
-		function(form, ctx){
-			var $left = this.left, $right = this.right;
-			return re($right, function(e){ return ctx(['=', $left, e]) })
-		}],
-	[['.begin', ',..args'], function(form, ctx){ return re$b(this.args, ctx) }],
-	[[['.', ',left', ',right'], ',..args'], function(form, ctx){
-		var $left = this.left, $right = this.right, $args = this.args;
-		return ra($left, function(xl){
-			return re($right, function(xr){
-				var t = newt();
-				return ['.begin', ['=', t, ['.', xl, xr]], re$($args, function(x$){
-					if(x$) return ctx([['.', t, ['.quote', 'call']], xl].concat(x$))
-					else return ctx([['.', t, ['.quote', 'call']], xl])
-				})]
-			})
-		})
-	}],
-	[[',fn', ',..args'], re$],
-	[any, function(form, ctx){ return ctx(form) }]
-)
-
-function ra(form, ctx){
-	return re(form, function(x){
-		if(typeof x === 'string' && x[0] === '_'){
-			return ctx(x)
-		} else {
-			var t = newt();
-			return ['.begin', ['=', t, x], ctx(t)]
-		}
-	})
-}
-function re$(form, ctx){
-	if(!form.length) return ctx(null)
-	return ra(form[0], function(x0){
-		return re$(form.slice(1), function(x$){
-			if(x$) return ctx([x0].concat(x$))
-			else return ctx([x0])
-		})
-	})
-}
-function re$b(form, ctx){
-	if(!form.length) return ctx(['.unit'])
-	else if(form.length === 1) return ra(form[0], ctx)
-	else return rs(form[0], function(x0){
-		return ['.begin', x0, re$b(form.slice(1), function(x$){
-			if(x$) return ctx(x$)
-			else return ctx(['.unit'])
-		})]
-	})
-}
-
-
-// CPS [& form] into 
-var cps = syntax_rule(
+	[empty, function(form, env, ctx){ return ctx['.unit'] }],
+	// Deferred nodes
 	[['&!', ',x'], function(form, env, ctx){
 		var t = newt();
-		return cps(this.x, env, function(x){
+		return re(this.x, env, function(x){
 			return ['.return', [x, ['.lambda', [t], ctx(t)]]]
 		})
 	}],
+	// Flow Controls, Deferred
 	[['&', '.return', ',x'], function(form, env, ctx){
-		return cps(this.x, env, function(x){
+		return re(this.x, env, function(x){
 			return ['.return', x]
 		})
 	}],
+	[['&', '.begin', ',x'], function(form, env, ctx){
+		return re(this.x, env, ctx)
+	}],
+	[['&', '.begin', ',x', ',..rest'], function(form, env, ctx){
+		var $rest = this.rest;
+		return ra(this.x, env, function(x){
+			return re(['&', '.begin'].concat($rest), env, function(v){
+				return ctx(v);
+			})
+		})
+	}],
 	[['&', '.if', ',test', ',consequent'], function(form, env, ctx){
-		return cps(form.concat([['.unit']]), env, ctx)
+		return re(form.concat([['.unit']]), env, ctx)
 	}],
 	[['&', '.if', ',test', ',consequent', ',alternate'], function(form, env, ctx){
 		var $test = this.test, $consequent = this.consequent, $alternate = this.alternate;
@@ -232,10 +142,10 @@ var cps = syntax_rule(
 		var tx = newt();
 		return ['.begin', 
 			['=', t, ['lambda', [tx], ctx(tx)]], 
-			cpsa($test, env, function(c){
+			ra($test, env, function(c){
 				return ['.if', c, 
-					cps($consequent, env, function(x){ return ['.return', [t, x]] }),
-					cps($alternate, env, function(x){ return ['.return', [t, x]] })
+					re($consequent, env, function(x){ return ['.return', [t, x]] }),
+					re($alternate, env, function(x){ return ['.return', [t, x]] })
 				];
 			})
 		];
@@ -248,73 +158,133 @@ var cps = syntax_rule(
 		var trx = newt();
 		return ['.begin', 
 			['=', t, ['.lambda', [tx], ctx(tx)]], 
-			['=', tr, ['.lambda', [trx], cpsa($test, env, function(c){
+			['=', tr, ['.lambda', [trx], ra($test, env, function(c){
 				return ['.if', c, 
-					cps($body, env, function(x){ return ['.return', [tr, x]] }),
+					re($body, env, function(x){ return ['.return', [tr, x]] }),
 					[t, trx]
 				];
 			})]],
 			['.return', [tr]]
 		];
 	}],
+	// Flow controls, Plain
+	[['.return', ',x'], function(form, env, ctx){
+		return re(this.x, env, function(x){ return ['.return', x] })
+	}],
+	[['.begin', ',..args'], function(form, env, ctx){ return re$b(this.args, env, ctx) }],
+	[['.if', ',cond', ',consequent'], function(form, env, ctx){
+		var $consequent = this.consequent;
+		return re(this.cond, env, function(c){
+			var t = newt();
+			return ['.begin', ['.if', c, re($consequent, env, function(x){ return ['=', t, x] })], ctx(t)]
+		});
+	}],
+	[['.if', ',cond', ',consequent', ',alternate'], function(form, env, ctx){
+		var $consequent = this.consequent;
+		var $alternate = this.alternate;
+		return re(this.cond, env, function(c){
+			var t = newt();
+			return ['.begin', ['.if', c, 
+				re($consequent, env, function(x){ return ['=', t, x] }),
+				re($alternate, env, function(x){ return ['=', t, x] })
+			], ctx(t)]
+		});
+	}],
+	[['.while', ',cond', ',body'], function(form, env, ctx){
+		var $cond = this.cond;
+		var $body = this.body;
+		return ra($cond, env, function(c){
+			var t = newt();
+			return ['.begin', ['.while', c, re($body, env, function(x){
+				return ['.begin', ['=', t, x], re($cond, env, function(x){ return ['=', c, x] })]
+			})], ctx(t)]
+		});
+	}],
+	// Expressions
+	[['.lambda', ',args', ',body'], function(form, env, ectx){ return ctx(form) }],
+	[['.quote', ',x'], function(form, env, ctx){ return ctx(form) }],
+	[['.unit'], function(form, env, ctx){ return ctx(form) }],
+	[['.plain', ',x'], function(form, env, ctx){ return ctx(this.x) }],
+
 	[	['&', '=', ['&', '.', ',obj', ',field'], ',right'], 
 		['&', '=', ['.plain', ['.', ',obj', ',field']], ',right'], 
-		['&', '=', ['.', ',obj', ',field'], ',right'], 
-		function(form, env, ctx){
+		['&', '=', ['.', ',obj', ',field'], ',right'],
+		['=', ['.', ',obj', ',field'], ',right'],
+		['=', ['.plain', ['.', ',obj', ',field']], ',right'], 
+		function (form, env, ctx){
 			var $obj = this.obj, $field = this.field, $right = this.right;
-			return cpsa($obj, env, function(o){
-				return cpsa($field, env, function(f){
-					return cps($right, env, function(r){
-						return ctx(['=', ['.', o, f], r])
+			return ra($obj, env, function(xl){
+				return ra($field, env, function(xr){
+					return re($right, env, function(r){
+						return ctx(['=', ['.', xl, xr], r])
 					})
 				})
 			})
 		}],
 	[	['&', '=', ['.plain', ',left'], ',right'],
 		['&', '=', ',left', ',right'], 
-		function(form, env, ctx){
+		['=', ['.plain', ',left'], ',right'], 
+		['=', ',left', ',right'], 
+		function (form, env, ctx){
 			var $left = this.left, $right = this.right;
-			return cps($right, env, function(r){ return ctx(['=', $left, r])})
+			return re($right, env, function(e){ return ctx(['=', $left, e]) })
 		}],
 	[	['&', ['.', ',left', ',right'], ',..args'],
 		['&', ['.plain', ['.', ',left', ',right']], ',..args'],
 		['&', ['&', '.', ',left', ',right'], ',..args'], 
+		[['.', ',left', ',right'], ',..args'], 
 		function (form, env, ctx){
 			var $left = this.left, $right = this.right, $args = this.args;
-			return cpsa($left, env, function(xl){
-				return cpsa($right, env, function(xr){
+			return ra($left, env, function(xl){
+				return re($right, env, function(xr){
 					var t = newt();
-					return ['.begin', ['=', t, ['.', xl, xr]],
-						cps$($args, env, function(x$){
-							return ctx([['.', t, ['.quote', 'call']], xl].concat(x$))
-						})
-					]
+					return ['.begin', ['=', t, ['.', xl, xr]], re$($args, env, function(x$){
+						if(x$) return ctx([['.', t, ['.quote', 'call']], xl].concat(x$))
+						else return ctx([['.', t, ['.quote', 'call']], xl])
+					})]
 				})
 			})
 		}],
-	[['&', ',callee', ',..args'], function(form, env, ctx){
-		var $args = this.args;
-		return cpsa(this.callee, env, function(x0){
-			return cps$($args, env, function(x$){
-				return ctx([x0].concat(x$))
+	[	['&', ',callee', ',..args'], 
+		[',callee', ',..args'], 
+		function(form, env, ctx){
+			var $args = this.args, $callee = this.callee;
+			return re($callee, env, function(x0){
+				return re$($args, env, function(x$){
+					return ctx([x0].concat(x$))
+				})
 			})
-		})
-	}],
-	[any, function(form, env, ctx){ return re(form, ctx) }]
+		}],
+	[any, function(form, env, ctx){ return ctx(form) }]
 );
-var cpsa = function(form, env, ctx){
-	return cps(form, env, function(x){
-//		if(typeof x === 'string' && x[0] === '_') return ctx(x);
-		var t = newt();
-		return ['.begin', ['=', t, x], ctx(t)]
+
+function ra(form, env, ctx){
+	return re(form, env, function(x){
+		if(typeof x === 'string' && x[0] === '_'){
+			return ctx(x)
+		} else {
+			var t = newt();
+			return ['.begin', ['=', t, x], ctx(t)]
+		}
 	})
 }
-function cps$(form, env, ctx){
-	if(!form.length) return ctx([])
-	return cpsa(form[0], env, function(x0){
-		return cps$(form.slice(1), env, function(x$){
-			return ctx([x0].concat(x$))
+function re$(form, env, ctx){
+	if(!form.length) return ctx(null)
+	return ra(form[0], env, function(x0){
+		return re$(form.slice(1), env, function(x$){
+			if(x$) return ctx([x0].concat(x$))
+			else return ctx([x0])
 		})
+	})
+}
+function re$b(form, env, ctx){
+	if(!form.length) return ctx(['.unit'])
+	else if(form.length === 1) return ra(form[0], env, ctx)
+	else return rs(form[0], env, function(x0){
+		return ['.begin', x0, re$b(form.slice(1), env, function(x$){
+			if(x$) return ctx(x$)
+			else return ctx(['.unit'])
+		})]
 	})
 }
 
@@ -330,6 +300,8 @@ function mb(form){
 			}
 		};
 		return ['.begin'].concat(res);
+	} else if(form instanceof Array && form[0] === '.plain') {
+		return mb(form[1])
 	} else if(form instanceof Array){
 		return form.map(mb)
 	} else {
@@ -338,5 +310,5 @@ function mb(form){
 }
 
 exports.pass = function(form){
-	return mb(rs(plain(form), id))
+	return mb(rs(plain(form), {}, id))
 }
